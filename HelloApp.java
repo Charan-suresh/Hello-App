a5575a45e0e02d3921a5f9e668326a940ef8bc20
Charan Suresh <charansuresh456@gmail.com>
2026-03-23 18:37:58 +0530

    Coded UC1: Display Hello World in console

---
+++ HelloApp.java
@@ -0,0 +1,11 @@
+/**
+ * OOPS Banner App
+ * UC1 - Display Hello World
+ * @author Charan
+ * @version 1.0
+ */
+public class HelloApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
